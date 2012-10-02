0c576b56a0834b5845521980cfba14e0a65e6fdc
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2012-10-02 12:20:10 +0200

    Stop after failure.

--- import.bat
+++ import.bat
@@ -1,4 +1,4 @@
-@git remote add bib git@github.com:Toxaris/Bib.git
-@git fetch bib
+@git remote add bib git@github.com:Toxaris/Bib.git && ^
+@git fetch bib && ^
 @git subtree add --squash --prefix bib --message "Import shared bibliography." bib/master
 
